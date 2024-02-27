64c4407ea1aeda2235ecdd4a061aaa57f74def9c
lioraft <lioraftabi@gmail.com>
2024-02-27 16:54:55 +0200

    revert to start

--- Cucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ Cucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -4,52 +4,16 @@ import io.cucumber.java.en.*;
 
 import org.junit.jupiter.api.Assertions.*;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
 public class StepDefinitions {
 
-    // set webdriver chrome path
-    private String driver_path = "C:\\Users\\Lior\\Desktop\\Semester 5\\SQE\\2023-mbt-08-41\\Selenium\\chromedriver.exe";
-    private String driver_name = "webdriver.chrome.driver";
-    private ChromeDriver driver;
-    private WebDriverWait wait;
-
     // $$*TODO* explain what this step does$$
-    @Given("User is on home page")
-    public void userIsOnHomePage() {
-        System.setProperty(driver_name, driver_path);
-        this.driver = new ChromeDriver();
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(40));
-        driver.get("http://localhost/opencart/");
-        driver.manage().window().setPosition(new Point(700, 5));
+    @Given("an example scenario")
+    public void anExampleScenario() {
     }
 
     // $$*TODO* explain what this step does$$
-    @When("User is logged in with {string} and {string}")
+    @When("all step definitions are implemented")
     public void allStepDefinitionsAreImplemented() {
-    }
-
-    @And("User adds a {string} to cart")
-    public void userAddsAProductToCart(String product) {
-    }
-
-    @And("User proceeds to checkout")
-    public void userProceedsToCheckout() {
     }
 
     // $$*TODO* explain what this step does$$
--- Cucumber/src/test/resources/hellocucumber/example.feature
+++ Cucumber/src/test/resources/hellocucumber/example.feature
@@ -1,6 +1,6 @@
 Feature: A set of scenarios for testing the "example" module
 
-  Scenario: User checks out a product
-    Given User is on home page
+  Scenario: Testing how a case where a user adds a product to the cart
+    Given an example scenario
     When all step definitions are implemented
     Then the scenario passes
